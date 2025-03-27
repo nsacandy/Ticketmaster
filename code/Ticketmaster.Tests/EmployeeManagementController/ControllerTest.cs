15682781f2bc26bef77e12a55917fb184148e9c9
Nicolas Sacandy <nicsacandy@gmail.com>
2025-03-27 14:17:04 -0400

    Working with tests
    Needed to commit to pull

--- code/Ticketmaster.Tests/EmployeeManagementController/ControllerTest.cs
+++ code/Ticketmaster.Tests/EmployeeManagementController/ControllerTest.cs
@@ -8,70 +8,92 @@ using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Ticketmaster.Tests.EmployeeManagementController
-
-//public class EmployeeManagementIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    //private readonly WebApplicationFactory<Program> _factory;
 
-    //public EmployeeManagementIntegrationTests(WebApplicationFactory<Program> factory)
-    //{
-    //    _factory = factory.WithWebHostBuilder(builder =>
-    //    {
-    //        builder.ConfigureServices(services =>
-    //        {
-    //            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TicketmasterContext>));
-    //            if (descriptor != null) services.Remove(descriptor);
-    //            services.AddDbContext<TicketmasterContext>(options => options.UseInMemoryDatabase("TestDb"));
-    //        });
-    //    });
+    public class EmployeeManagementIntegrationTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public EmployeeManagementIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            var server = new WebApplicationFactory<>()
+        }
+
+        [Fact]
+        public async Task Get_EmployeeManagement()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.GetAsync("/EmployeeManagement");
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("EmployeeManagement", responseString);
+        }
+
+        [Fact]
+        public async Task Get_EmployeeManagement_Create()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.GetAsync("/EmployeeManagement/Create");
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Create", responseString);
+        }
+
+        [Fact]
+        public async Task Post_EmployeeManagement_Create()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.PostAsync("/EmployeeManagement/Create", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("Name", "Test Employee")
+            }));
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Test Employee", responseString);
+        }
+
+        [Fact]
+        public async Task Get_EmployeeManagement_Edit()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.GetAsync("/EmployeeManagement/Edit/1");
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Edit", responseString);
+        }
+
+        [Fact]
+        public async Task Post_EmployeeManagement_Edit()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            // Act
+            var response = await client.PostAsync("/EmployeeManagement/Edit/1", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("Name", "Test Employee Updated")
+            }));
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Test Employee Updated", responseString);
+        }
+
+        [Fact]
+        public async Task Get_EmployeeManagement_Delete()
+        {
+            // Arrange
+        }
     }
-
-//    [Fact]
-//    public async Task Index_ReturnsSuccessAndCorrectView()
-//    {
-//        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
-//        var response = await client.GetAsync("/EmployeeManagement");
-//        response.EnsureSuccessStatusCode();
-//    }
-
-//    [Fact]
-//    public async Task StageEmployeeAdd_AddsEmployeeToSession()
-//    {
-//        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
-//        var content = new FormUrlEncodedContent(new[]
-//        {
-//            new KeyValuePair<string, string>("id", "1"),
-//            new KeyValuePair<string, string>("firstName", "John"),
-//            new KeyValuePair<string, string>("lastName", "Doe"),
-//            new KeyValuePair<string, string>("email", "johndoe@example.com"),
-//            new KeyValuePair<string, string>("pword", "password123"),
-//            new KeyValuePair<string, string>("phoneNum", "1234567890"),
-//            new KeyValuePair<string, string>("eRole", "Employee")
-//        });
-//        var response = await client.PostAsync("/EmployeeManagement/StageEmployeeAdd", content);
-//        response.EnsureSuccessStatusCode();
-//    }
-
-//    [Fact]
-//    public async Task CommitChanges_SavesEmployeeToDatabase()
-//    {
-//        using var scope = _factory.Services.CreateScope();
-//        var context = scope.ServiceProvider.GetRequiredService<TicketmasterContext>();
-
-//        context.Employee.Add(new Employee
-//        {
-//            Id = 1,
-//            FirstName = "John",
-//            LastName = "Doe",
-//            Email = "johndoe@example.com",
-//            Pword = "password123",
-//            PhoneNum = "1234567890",
-//            ERole = "Employee"
-//        });
-//        await context.SaveChangesAsync();
-
-//        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
-//        var response = await client.PostAsync("/EmployeeManagement/CommitChanges", null);
-//        response.EnsureSuccessStatusCode();
-//    }
-//}
+}
