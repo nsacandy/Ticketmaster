8e0d2336f225245f5fd80c05ccc56315ed7e1639
Nate Schaab <nateschaab@gmail.com>
2025-04-16 13:36:54 -0400

    Add: Moving tasks.
    Tasks can now be moved to different stages on a project board.

---
+++ code/Ticketmaster/Migrations/20250416171044_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketmaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Pword",
+                value: "AQAAAAIAAYagAAAAEAhyn1Y3U4qD1TLISIlX+8cIbAZJy8qU//OQlRgNhDl4gWBHBoTBIvniZg/YptkwGA==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Pword",
+                value: "AQAAAAIAAYagAAAAENjHEpOgSW4plLWvF2f76tsPNQ+jRE1GURqVsw1gHBb/8IQJ7MqDMOhbpHAztrmN6g==");
+        }
+    }
+}
