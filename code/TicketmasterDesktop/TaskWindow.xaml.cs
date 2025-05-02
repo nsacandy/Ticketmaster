45fa82f9d0565d0d360fe08e89f142c1fc4688dd
Nicolas Sacandy <nicsacandy@gmail.com>
2025-05-02 02:15:31 -0400

    Added self assign/unassign
    Not sure what happens when you sign in with a different user. Also need to add moving tasks to different stages and then done

--- code/TicketmasterDesktop/TaskWindow.xaml.cs
+++ code/TicketmasterDesktop/TaskWindow.xaml.cs
@@ -40,27 +40,71 @@ namespace TicketmasterDesktop
                     .Where(t => t.StageId == stage.StageId)
                     .ToList();
 
+                var taskPanel = new StackPanel();
+
+                foreach (var task in tasks)
+                {
+                    string assignedName = "Unassigned";
+                    if (task.AssignedTo.HasValue)
+                    {
+                        var employee = context.Employee.FirstOrDefault(e => e.Id == task.AssignedTo.Value);
+                        if (employee != null)
+                        {
+                            assignedName = employee.FirstName + " " + employee.LastName;
+                        }
+                    }
+
+                    var taskRow = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(5) };
+                    taskRow.Children.Add(new TextBlock
+                    {
+                        Text = $"{task.Title}: {task.Description} (Assigned to: {assignedName})",
+                        Width = 400,
+                        VerticalAlignment = VerticalAlignment.Center
+                    });
+
+                    var assignButton = new Button
+                    {
+                        Content = task.AssignedTo == Session.CurrentUser?.Id ? "Unassign" : "Assign",
+                        Tag = task,
+                        Margin = new Thickness(10, 0, 0, 0)
+                    };
+                    assignButton.Click += AssignOrUnassign_Click;
+                    taskRow.Children.Add(assignButton);
+
+                    taskPanel.Children.Add(taskRow);
+                }
+
                 var stageGroup = new GroupBox
                 {
                     Header = stage.StageTitle,
                     Margin = new Thickness(10),
-                    Content = new ListBox
-                    {
-                        ItemsSource = tasks,
-                        DisplayMemberPath = "Title",
-                        Tag = stage // Store stage for reference
-                    }
+                    Content = taskPanel
                 };
 
                 StagesPanel.Children.Add(stageGroup);
             }
         }
 
-        private void BackToProjectsButton_Click(object sender, RoutedEventArgs e)
+        private void AssignOrUnassign_Click(object sender, RoutedEventArgs e)
         {
-            var projectList = new ProjectListWindow(Session.CurrentUser.Id);
-            projectList.Show();
-            this.Close();
+            if (sender is Button button && button.Tag is TaskItem task)
+            {
+                using var context = new TicketmasterContext(App.DbOptions);
+                var dbTask = context.TaskItem.FirstOrDefault(t => t.TaskItemId == task.TaskItemId);
+                if (dbTask != null)
+                {
+                    if (dbTask.AssignedTo == Session.CurrentUser?.Id)
+                    {
+                        dbTask.AssignedTo = null;
+                    }
+                    else
+                    {
+                        dbTask.AssignedTo = Session.CurrentUser?.Id;
+                    }
+                    context.SaveChanges();
+                    LoadBoardData(); // Refresh UI
+                }
+            }
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
@@ -71,6 +115,15 @@ namespace TicketmasterDesktop
             this.Close();
         }
 
-
+        private void BackToProjectsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var projectList = new ProjectListWindow(Session.CurrentUser.Id);
+            projectList.Show();
+            this.Close();
+        }
     }
 }
+
+
+
+
